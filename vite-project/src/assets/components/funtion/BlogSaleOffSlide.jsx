8a1d7da4c822ac9b7479dc2717cae8b70cd0e83f
Trung Nguyen <trungnguyen1221999@gmail.com>
2025-08-31 20:54:41 +0300

    Update BlogSaleOffSlide.jsx

--- vite-project/src/assets/components/funtion/BlogSaleOffSlide.jsx
+++ vite-project/src/assets/components/funtion/BlogSaleOffSlide.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import createBlogSlide from "./createBlogSlide";
 import BlogSlideData from "../data/BlogSlideData";
+import styled from "styled-components";
 
 const BlogSaleOffSlide = () => {
   return (
-    <>
+    <StyledContainer>
       {createBlogSlide({
         data: BlogSlideData,
         heading: "Get your everyday life in order with up to 50% off",
         btn: "For shopping",
       })}
-    </>
+    </StyledContainer>
   );
 };
 
 export default BlogSaleOffSlide;
+const StyledContainer = styled.div`
+  margin-top: 13rem;
+`;
